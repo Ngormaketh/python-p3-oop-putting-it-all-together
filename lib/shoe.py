08961144b9e9fad12cd8edf2569d3829787e9912
Anyang Diing <anyang.ngor@student.moringaschool.com>
2025-04-10 21:58:51 +0300

    oop-putting-it-all-together-complete

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size # Assuming initial validation is not required here
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+            self._size = None  # You can choose to raise an exception instead of setting it to None
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
